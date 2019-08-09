2ce2e07f7887c7dec69f2bad947a992f5f918ada
Bruno <brunob.inacio@gmail.com>
2019-08-09 02:28:02 -0300

    Add Assembler project

---
+++ src/assembler/interface.cpp
@@ -0,0 +1,70 @@
+/**
+* interface.cpp
+* PCS 3216 - Sistemas de Programação - 2019
+* Bruno Brandão Inácio
+*/
+
+#include "interface.hpp"
+#include "assembler.hpp"
+
+#include <iostream>
+#include <fstream>
+
+
+/**
+ * Inicia a interface.
+ */
+void Interface::start() {
+	std::cout << "Bem vindo ao assembler!" << std::endl;
+	this->menu();
+
+	return;
+}
+
+
+/**
+ * Inicializa o assembler.
+ */
+void Interface::menu() {
+	try {
+		std::string fileName = this->inputFile();
+
+		Assembler assembler(fileName);
+		assembler.assemble();
+	}
+	catch (std::string e) {
+		std::cout << e << std::endl;
+		return;
+	}
+
+	std::cout << "\nO codigo foi montado com sucesso." << std::endl;
+	return;
+}
+
+
+/**
+ * Controla a entrada de um arquivo.
+ */
+std::string Interface::inputFile() {
+	std::string fileName;
+
+	std::cout << "Digite o nome do arquivo (dentro da pasta programs): ";
+	std::cin >> fileName;
+	fileName = "programs/" + fileName;
+
+	if (!std::ifstream(fileName))
+		throw std::string("\nO arquivo nao pode ser aberto.\n");
+
+	return fileName;
+}
+
+
+/**
+* Realiza a chamada para a interface.
+*/
+int main() {
+	Interface pc;
+	pc.start();
+
+	return 0;
+}
---
+++ src/assembler/interface.hpp
@@ -0,0 +1,15 @@
+/**
+* interface.hpp
+* PCS 3216 - Sistemas de Programação - 2019
+* Bruno Brandão Inácio
+*/
+
+#pragma once
+#include <string>
+
+struct Interface {
+	void start();
+	void menu();
+
+	static std::string inputFile();
+};
